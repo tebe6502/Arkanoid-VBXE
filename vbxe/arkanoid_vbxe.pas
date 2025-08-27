52710ca820a45990e26b5273f5b702911e8667aa
tebe6502 <tebe6502@o2.pl>
2025-08-27 23:01:12 +0200

    update

--- vbxe/arkanoid_vbxe.pas
+++ vbxe/arkanoid_vbxe.pas
@@ -1,4 +1,49 @@
-program arkanoid;//: $1000;
+
+{ ----------------------------------------------------------------------- }
+{                                                                         }
+{                                 ARKANOID                                }
+{                         Written by Claudio Bazzan                       }
+{                     Esame di Fondamenti di Informatica I                }
+{                         Professor Eduardo Calabrese                     }
+{                    Revisione v1.02 per il publico dominio               }
+{                                                                         }
+{ Nota: Questo programma e' la riproduzione fedele (per quanto possibile) }
+{       del famoso Coin-Op da bar Arkanoid. I diritti del gioco originale }
+{       sono della Taito Corp, questa versione e' da intendersi solamente }
+{       a titolo dimostrativo. E' quindi assolutamente vietata la vendita }
+{       o qualsiasi altro scopo di lucro.                                 }
+{                                                                         }
+{       Il programma gira su tutte le macchine con processore 80286 o su- }
+{       periore. E' consigliato l'utilizzo di un 80386 per aprezzarne a   }
+{       pieno le qualita'.                                                }
+{                                                                         }
+{       E sconsigliata la compilazione con RANGE CHECKING attivato dal    }
+{       momento che i tempi di esecuzione sono critici e un minimo ritar- }
+{       do nell'esecuzione di alcuni punti puo' provocarne un significa-  }
+{       tivo e fastidioso rallentamento globale.                          }
+{       Naturalmente il programma funziona tranquillamente anche con      }
+{       la verifica degli intervalli attiva.                              }
+{                                                                         }
+{ ----------------------------------------------------------------------- }
+
+{ NOTA PER CHI CERCASSE SPECIFICHE RIGUARDO ALLA GRAFICA                  }
+{                                                                         }
+{ La grafica di questo programma e' stata progettata con POWERDESIGN 386, }
+{ un programma shareware che permette di manipolare immagini in modo      }
+{ avanzato. Chiunque fosse interessato puo' prelevarlo gratuitamente da   }
+{ qualunque nodo ISN oppure a Banana's BBS al 2:332/508 (Tel.0521/904046).}
+{ Il programma viene continuamente aggiornato, l'ultima versione disponi- }
+{ bile al momento della stesura di questo testo e' la v1.06 beta.         }
+{ Il nome con cui e' possibile prelevare tale programma e' PD386Bxx.ARJ o }
+{ PD386Bxx.ZIP ove al posto delle xx viene messo il numero della versione }
+{ corrente. Pertanto un eventuale DOWNLOAD puo' essere fatto col nome:    }
+{ PD386B*.*                                                               }
+{ Il programma richiede un processore 386 (o migliore), scheda VGA o SVGA }
+{ ed e' consigliabile la presenza di memoria estesa/espansa e relativo    }
+{ driver di memoria (es. EMM386, QEMM, ecc.)                              }
+
+
+program arkanoid;
 
 uses crt, atari, vbxe, joystick;
 
